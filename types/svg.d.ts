f7668d4db72d8040abbe4e073dbb2b12b190f030
Chloe <gtalk.with.nani@gmail.com>
2024-12-17 07:03:08 +0900

    Update svg.d.ts

--- types/svg.d.ts
+++ types/svg.d.ts
@@ -1,5 +1,5 @@
 declare module '*.svg' {
   import React from 'react';
-  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGElement>>;
   export default ReactComponent;
 }
